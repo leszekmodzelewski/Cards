cf3d027b254de8a334edfec65086f7b29c9593b8
Dariusz Konieczka <dariusz.konieczka@aveva.com>
2021-04-12 18:20:26 +0200

    changes before Leszek send his changes

--- Entities/Table/Cmd_TblCalculate.cs
+++ Entities/Table/Cmd_TblCalculate.cs
@@ -274,17 +274,17 @@ namespace GeoLib.Entities.Table
 
             if (attRef.Tag == "X_4")
             {
-                EntityBaseUtils.UpdateNullableDoubleAttribute(attRef, matchedPoint.RealPoint.X - matchedPoint.TheoryPoint.X);
+                EntityBaseUtils.UpdateNullableDoubleAttribute(attRef, Math.Abs(matchedPoint.RealPoint.X - matchedPoint.TheoryPoint.X));
             }
 
             if (attRef.Tag == "Y_4")
             {
-                EntityBaseUtils.UpdateNullableDoubleAttribute(attRef, matchedPoint.RealPoint.Y - matchedPoint.TheoryPoint.Y);
+                EntityBaseUtils.UpdateNullableDoubleAttribute(attRef, Math.Abs(matchedPoint.RealPoint.Y - matchedPoint.TheoryPoint.Y));
             }
 
             if (attRef.Tag == "Z_4")
             {
-                EntityBaseUtils.UpdateNullableDoubleAttribute(attRef, matchedPoint.RealPoint.Z - matchedPoint.TheoryPoint.Z);
+                EntityBaseUtils.UpdateNullableDoubleAttribute(attRef, Math.Abs(matchedPoint.RealPoint.Z - matchedPoint.TheoryPoint.Z));
             }
         }
 
