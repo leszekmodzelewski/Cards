c129ffccad8b1c5e7e1aae6bea2234dc9f5adbce
Dariusz Konieczka <dariusz.konieczka@aveva.com>
2022-01-21 19:43:09 +0100

    c2

---
+++ Commands/Cmd_Clear.cs
@@ -0,0 +1,15 @@
+﻿using GeoLib.Logic;
+using ZwSoft.ZwCAD.Runtime;
+
+namespace GeoLib.Entities.Table
+{
+    public class Cmd_Clear : Cmd_TableBase
+    {
+        [CommandMethod("CLEAR", CommandFlags.UsePickSet)]
+        public void ClearData()
+        {
+            Points.Clear();
+            Cmd_TableRegen.Refresh();
+        }
+    }
+}
